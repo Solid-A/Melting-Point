01dc045688471806cae4e6c1262911671cff2028
Ahmed Farouk <solid.ahmed85@gmail.com>
2024-09-09 16:49:47 +0300

    New cascade sheet

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let outer = document.getElementById("outer");
 let outer2 = document.getElementById("outer2");
-let click = document.getElementById("click");
+let click = document.getElementById("button");
 
 click.addEventListener( "click", function() { 
     // Get the value of the input field and store it in a variable called 'inputValue'
@@ -17,20 +17,21 @@ click.addEventListener( "click", function() {
     let bar = document.getElementById("bar").value;
     let dolo = document.getElementById("dolo").value;
     let cole = document.getElementById("cole").value;
-    let zin = document.getElementById("zin").value;
-    let zir = document.getElementById("zir").value;
+    let zin= document.getElementById("zin").value;
+    let zir_s = document.getElementById("zir-s").value;
+    let zir_o = document.getElementById("zir-o").value;
     let bor = document.getElementById("bor").value;
     let tita = document.getElementById("tita").value;
     let silli = document.getElementById("silli").value;
     let mulli = document.getElementById("mulli").value;
     let kao = document.getElementById("kao").value;
 
-    let arr = [silica, quartz, po_feld, so_feld, ca_feld, ca_ca, so_ca, po_ca, alu, bar, dolo, cole, zin, zir, bor, tita, silli, mulli, kao];
-    let arr2 = [1710, 1710, 1300, 1300, 1500, 1200, 1200, 1300, 2000, 1200, 1200, 1000, 2000, 1855, 850, 2000, 1550, 1810, 1500];
+    let arr = [silica, quartz, po_feld, so_feld, ca_feld, ca_ca, so_ca, po_ca, alu, bar, dolo, cole, zin, zir_s, zir_o, bor, tita, silli, mulli, kao];
+    let arr2 = [1720, 1720, 1200, 1150, 1550, 825, 851, 891, 2072, 1923, 1170, 1050, 1800, 2550, 2715, 850, 1870, 1550, 1810, 1500];
     
     let sum = 0;
     let sum2 = 0;
-    for (let i=0; i<20; i++) {
+    for (let i=0; i<=20; i++) {
         if (!Number(arr[i])) {
             continue;
         } else {
@@ -40,10 +41,10 @@ click.addEventListener( "click", function() {
     }
     if (sum2 <= 0) {
         alert ("Please make sure that you have entered number > 0 .");
-        return outer.innerText = 0 + ' c', outer2.innerText = 0 + ' kg';;
+        return;
     }
-    return outer.innerText = parseInt(sum / sum2) + ' c', outer2.innerText = sum2 + ' kg';
+    let sum3 = parseInt(sum / sum2);
+    return outer.innerText = `${sum3} \u00B0c`, outer2.innerText = sum2 + ' kg';
 });
-
 
 
